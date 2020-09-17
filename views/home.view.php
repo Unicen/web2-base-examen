60e20444eab3e54fdaa09551153c1b3b48fca306
Franco Stramana <franco.stramana@gmail.com>
2020-09-17 09:33:54 -0300

    initial commit

---
+++ views/home.view.php
@@ -0,0 +1,12 @@
+<?php
+require_once('libs/smarty/Smarty.class.php');
+
+class HomeView {
+
+    private $smarty;
+
+    public function __construct() {
+        $this->smarty = new Smarty();
+    }
+
+}
