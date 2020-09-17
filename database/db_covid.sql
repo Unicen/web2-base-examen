0b4d686d3002a4a91b573e45dc4977823dc66f24
Franco Stramana <franco.stramana@gmail.com>
2020-09-17 09:37:49 -0300

    update database

--- database/db_covid.sql
+++ database/db_covid.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-06-2020 a las 00:57:26
--- Versión del servidor: 10.1.40-MariaDB
--- Versión de PHP: 7.3.5
+-- Tiempo de generación: 17-09-2020 a las 14:14:02
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_covid`
+-- Base de datos: `covidddbb`
 --
 
 -- --------------------------------------------------------
@@ -30,27 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `centro` (
   `id_centro` int(11) NOT NULL,
-  `tipo` varchar(100) NOT NULL,
-  `nombre_centro` varchar(100) NOT NULL,
-  `cant_camas` int(11) NOT NULL,
-  `cant_camas_ocupadas` int(11) NOT NULL DEFAULT '0',
-  `id_ciudad_fk` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tipo` varchar(200) NOT NULL,
+  `nombre_centro` varchar(200) NOT NULL,
+  `id_ciudad_fk` int(11) NOT NULL,
+  `cant_camas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `centro`
 --
 
-INSERT INTO `centro` (`id_centro`, `tipo`, `nombre_centro`, `cant_camas`, `cant_camas_ocupadas`, `id_ciudad_fk`) VALUES
-(1, 'hospital', 'Ramon Santamarina', 100, 10, 1),
-(2, 'clinica', 'Santa Maria', 30, 0, 2),
-(3, 'hospital', 'Garranhan', 150, 30, 4),
-(4, 'sanatorio', 'Pradera', 200, 70, 5),
-(5, 'sanatorio', 'Tandil', 40, 25, 1),
-(6, 'hospital', 'Centro Azul', 100, 30, 4),
-(7, 'sanatorio', 'Sanatorio Avenida', 500, 260, 4),
-(8, 'hospital', 'Verellen', 60, 10, 1),
-(9, 'sanatorio', 'Aires', 10, 2, 2);
+INSERT INTO `centro` (`id_centro`, `tipo`, `nombre_centro`, `id_ciudad_fk`, `cant_camas`) VALUES
+(1, 'hospital', 'Hospital Santamarina', 1, 20),
+(2, 'Sanatorio ', 'Sanatorio Tandil', 1, 12),
+(3, 'Clinica', 'Hispano', 3, 11),
+(4, 'Hospital', 'Olavarria', 2, 14);
 
 -- --------------------------------------------------------
 
@@ -60,21 +53,49 @@ INSERT INTO `centro` (`id_centro`, `tipo`, `nombre_centro`, `cant_camas`, `cant_
 
 CREATE TABLE `ciudad` (
   `id_ciudad` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `zona_riesgo` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nombre` varchar(200) NOT NULL,
+  `zona_riesgo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `ciudad`
 --
 
 INSERT INTO `ciudad` (`id_ciudad`, `nombre`, `zona_riesgo`) VALUES
-(1, 'Tandil', 0),
-(2, 'Rauch', 0),
-(3, 'Lobería', 0),
-(4, 'CABA', 1),
-(5, 'Mar del Plata', 1),
-(6, 'Olavarría', 1);
+(1, 'Tandil', 1),
+(2, 'Olavarria', 1),
+(3, 'Tres Arroyos', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ocupacion_diaria`
+--
+
+CREATE TABLE `ocupacion_diaria` (
+  `id_ocupacion` int(11) NOT NULL,
+  `id_centro_fk` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `cant_camas_ocupadas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ocupacion_diaria`
+--
+
+INSERT INTO `ocupacion_diaria` (`id_ocupacion`, `id_centro_fk`, `fecha`, `cant_camas_ocupadas`) VALUES
+(1, 1, '2020-09-16', 5),
+(2, 1, '2020-09-17', 6),
+(3, 1, '2020-09-15', 7),
+(4, 2, '2020-09-15', 8),
+(5, 2, '2020-09-16', 7),
+(6, 2, '2020-09-17', 6),
+(7, 3, '2020-09-15', 4),
+(8, 3, '2020-09-16', 4),
+(9, 3, '2020-09-17', 4),
+(10, 4, '2020-09-15', 9),
+(11, 4, '2020-09-16', 11),
+(12, 4, '2020-09-17', 13);
 
 --
 -- Índices para tablas volcadas
@@ -84,14 +105,19 @@ INSERT INTO `ciudad` (`id_ciudad`, `nombre`, `zona_riesgo`) VALUES
 -- Indices de la tabla `centro`
 --
 ALTER TABLE `centro`
-  ADD PRIMARY KEY (`id_centro`),
-  ADD KEY `id_ciudad_fk` (`id_ciudad_fk`);
+  ADD PRIMARY KEY (`id_centro`);
 
 --
 -- Indices de la tabla `ciudad`
 --
 ALTER TABLE `ciudad`
   ADD PRIMARY KEY (`id_ciudad`);
+
+--
+-- Indices de la tabla `ocupacion_diaria`
+--
+ALTER TABLE `ocupacion_diaria`
+  ADD PRIMARY KEY (`id_ocupacion`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -101,23 +127,19 @@ ALTER TABLE `ciudad`
 -- AUTO_INCREMENT de la tabla `centro`
 --
 ALTER TABLE `centro`
-  MODIFY `id_centro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_centro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `ciudad`
 --
 ALTER TABLE `ciudad`
-  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT de la tabla `ocupacion_diaria`
 --
-
---
--- Filtros para la tabla `centro`
---
-ALTER TABLE `centro`
-  ADD CONSTRAINT `centro_ibfk_1` FOREIGN KEY (`id_ciudad_fk`) REFERENCES `ciudad` (`id_ciudad`);
+ALTER TABLE `ocupacion_diaria`
+  MODIFY `id_ocupacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
